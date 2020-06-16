d1a8f7632af32cc686c14cac90a92f798a0d7396
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-16 08:14:43 -0400

    initial commit

---
+++ RubyBasics.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+
+# comment
+#print "Enter a value: "
+
+#first_num = gets.to_i
+
+#print "Enter another value: "
+
+#second_num = gets.to_i
+
+#puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+
+num_one = 1.000
+
+num99 = 0.999
+
+puts num_one.to_s + " + " + num99.to_s + " = " + (num_one + num99).to_s
+
+puts 1.class
+puts 1.234.class
+puts "A string".class
+
+#A_CONSTANT = 41.3
+
+A_CONSTANT = 1.6
+
+puts A_CONSTANT
+
+=begin
+write_handler = File.open("testFile.txt", "w")
+
+write_handler.puts("Random text here...").to_s
+
+write_handler.close
+=end
