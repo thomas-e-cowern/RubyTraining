166c65e69bf9254ed3394fe5cc9b478d91b74242
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-16 13:33:37 -0400

    updated arrays

--- Arrays.rb
+++ Arrays.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/ruby
 
+numbers = [1,2,3,4,5,6,7]
 
+for number in numbers
+    puts "#{number}, "
+end
+
+groceries = ["bananas", "potatoes", "pasta"]
+
+groceries.each do |food|
+    puts "#{food}, "
+end
+
+(0..5).each do |i|
+    puts "#{i}, "
+end
